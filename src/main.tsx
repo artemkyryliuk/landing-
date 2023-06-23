8cf31d94ca5879ac3c8e8f6d976004380ab44ec1
Artem Kyryliuk <no4ta.aptema@gmail.com>
2023-06-23 15:19:38 +0300

    feat: reset css styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { MantineProvider } from '@mantine/core'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.tsx'
 import './styles/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 )
