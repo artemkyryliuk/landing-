d81ae6ee9d46e81d01f5dcfa1e59a10d990709e4
Artem Kyryliuk <no4ta.aptema@gmail.com>
2023-06-23 15:25:49 +0300

    fix: added type

--- src/components/Features/WhyWe/why-we.ts
+++ src/components/Features/WhyWe/why-we.ts
@@ -1,4 +1,4 @@
-export const whyWe = [
+export const whyWe: string[] = [
   `Max flexibility — no more being shackled to SoWs or expensive change requests.`,
   `No more waiting for availability — while your competition pushes ahead.`,
   `No hiring costs — managing or training an entire design team. We're here when you need us.`,
