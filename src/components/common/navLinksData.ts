10c86cd15238f84bea6eee90911b28bfb8cb7c66
Artem Kyryliuk <no4ta.aptema@gmail.com>
2023-06-23 15:32:05 +0300

    fix: added type

--- src/components/common/navLinksData.ts
+++ src/components/common/navLinksData.ts
@@ -1,4 +1,4 @@
-export const navigation = [
+export const navigation: { href: string; text: string }[] = [
   { href: '#features', text: `Features` },
   { href: '#howitworks', text: `How it works` },
   { href: '#pricing', text: `Pricing` },
